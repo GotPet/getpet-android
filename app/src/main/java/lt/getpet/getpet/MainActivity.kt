0886e937716bf18573b0fb4dd868c7a16cfef873
GretaGri <greta.klimaite@gmail.com>
2018-10-20 14:43:11 +0300

    Added swipeable layout

--- app/src/main/java/lt/getpet/getpet/MainActivity.kt
+++ app/src/main/java/lt/getpet/getpet/MainActivity.kt
@@ -2,11 +2,238 @@ package lt.getpet.getpet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.yuyakaido.android.cardstackview.CardStackView
+import android.widget.ProgressBar
+import com.yuyakaido.android.cardstackview.SwipeDirection
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
+import android.text.method.TextKeyListener.clear
+import android.R.menu
+import android.os.Handler
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import lt.getpet.getpet.R.layout.activity_main
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    private var adapter: PetAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setup();
+        reload();
     }
+
+   override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.activity_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            R.id.menu_activity_main_reload -> reload()
+            R.id.menu_activity_main_add_first -> addFirst()
+            R.id.menu_activity_main_add_last -> addLast()
+            R.id.menu_activity_main_remove_first -> removeFirst()
+            R.id.menu_activity_main_remove_last -> removeLast()
+            R.id.menu_activity_main_swipe_left -> swipeLeft()
+            R.id.menu_activity_main_swipe_right -> swipeRight()
+            R.id.menu_activity_main_reverse -> reverse()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun createTouristSpot(): Pet {
+        return Pet("Yasaka Shrine", "Kyoto", "https://source.unsplash.com/Xq1ntWruZQI/600x800")
+    }
+
+    private fun createTouristSpots(): List<Pet> {
+        val spots = ArrayList<Pet>()
+        spots.add(Pet("Yasaka Shrine", "Kyoto", "https://source.unsplash.com/Xq1ntWruZQI/600x800"))
+        spots.add(Pet("Fushimi Inari Shrine", "Kyoto", "https://source.unsplash.com/NYyCqdBOKwc/600x800"))
+        spots.add(Pet("Bamboo Forest", "Kyoto", "https://source.unsplash.com/buF62ewDLcQ/600x800"))
+        spots.add(Pet("Brooklyn Bridge", "New York", "https://source.unsplash.com/THozNzxEP3g/600x800"))
+        spots.add(Pet("Empire State Building", "New York", "https://source.unsplash.com/USrZRcRS2Lw/600x800"))
+        spots.add(Pet("The statue of Liberty", "New York", "https://source.unsplash.com/PeFk7fzxTdk/600x800"))
+        spots.add(Pet("Louvre Museum", "Paris", "https://source.unsplash.com/LrMWHKqilUw/600x800"))
+        spots.add(Pet("Eiffel Tower", "Paris", "https://source.unsplash.com/HN-5Z6AmxrM/600x800"))
+        spots.add(Pet("Big Ben", "London", "https://source.unsplash.com/CdVAUADdqEc/600x800"))
+        spots.add(Pet("Great Wall of China", "China", "https://source.unsplash.com/AWh9C-QjhE4/600x800"))
+        return spots
+    }
+
+    private fun createTouristSpotCardAdapter(): PetAdapter {
+        val adapter = PetAdapter(applicationContext)
+        adapter.addAll(createTouristSpots())
+        return adapter
+    }
+
+    private fun setup() {
+        activity_main_card_stack_view.setCardEventListener(object : CardStackView.CardEventListener {
+            override fun onCardDragging(percentX: Float, percentY: Float) {
+                Log.d("CardStackView", "onCardDragging")
+            }
+
+            override fun onCardSwiped(direction: SwipeDirection) {
+                Log.d("CardStackView", "onCardSwiped: " + direction.toString())
+                Log.d("CardStackView", "topIndex: " + activity_main_card_stack_view.topIndex)
+                if (activity_main_card_stack_view.topIndex === adapter!!.getCount() - 5) {
+                    Log.d("CardStackView", "Paginate: " + activity_main_card_stack_view.topIndex)
+                    paginate()
+                }
+            }
+
+            override fun onCardReversed() {
+                Log.d("CardStackView", "onCardReversed")
+            }
+
+            override fun onCardMovedToOrigin() {
+                Log.d("CardStackView", "onCardMovedToOrigin")
+            }
+
+            override fun onCardClicked(index: Int) {
+                Log.d("CardStackView", "onCardClicked: $index")
+            }
+        })
+    }
+
+    private fun reload() {
+        activity_main_card_stack_view.setVisibility(View.GONE)
+        activity_main_progress_bar.setVisibility(View.VISIBLE)
+        Handler().postDelayed(Runnable {
+            adapter = createTouristSpotCardAdapter()
+            activity_main_card_stack_view.setAdapter(adapter)
+            activity_main_card_stack_view.setVisibility(View.VISIBLE)
+            activity_main_progress_bar.setVisibility(View.GONE)
+        }, 1000)
+    }
+
+    private fun extractRemainingTouristSpots(): LinkedList<Pet> {
+        val spots = LinkedList<Pet>()
+        for (i in activity_main_card_stack_view.getTopIndex() until adapter!!.count) {
+            spots.add(adapter!!.getItem(i))
+        }
+        return spots
+    }
+
+    private fun addFirst() {
+        val spots = extractRemainingTouristSpots()
+        spots.addFirst(createTouristSpot())
+        adapter!!.clear()
+        adapter!!.addAll(spots)
+        adapter!!.notifyDataSetChanged()
+    }
+
+    private fun addLast() {
+        val spots = extractRemainingTouristSpots()
+        spots.addLast(createTouristSpot())
+        adapter!!.clear()
+        adapter!!.addAll(spots)
+        adapter!!.notifyDataSetChanged()
+    }
+
+    private fun removeFirst() {
+        val spots = extractRemainingTouristSpots()
+        if (spots.isEmpty()) {
+            return
+        }
+
+        spots.removeFirst()
+        adapter!!.clear()
+        adapter!!.addAll(spots)
+        adapter!!.notifyDataSetChanged()
+    }
+
+    private fun removeLast() {
+        val spots = extractRemainingTouristSpots()
+        if (spots.isEmpty()) {
+            return
+        }
+
+        spots.removeLast()
+        adapter!!.clear()
+        adapter!!.addAll(spots)
+        adapter!!.notifyDataSetChanged()
+    }
+
+    private fun paginate() {
+        activity_main_card_stack_view.setPaginationReserved()
+        adapter!!.addAll(createTouristSpots())
+        adapter!!.notifyDataSetChanged()
+    }
+
+    fun swipeLeft() {
+        val spots = extractRemainingTouristSpots()
+        if (spots.isEmpty()) {
+            return
+        }
+
+        val target = activity_main_card_stack_view.getTopView()
+        val targetOverlay = activity_main_card_stack_view.getTopView().overlayContainer
+
+        val rotation = ObjectAnimator.ofPropertyValuesHolder(
+                target, PropertyValuesHolder.ofFloat("rotation", -10f))
+        rotation.duration = 200
+        val translateX = ObjectAnimator.ofPropertyValuesHolder(
+                target, PropertyValuesHolder.ofFloat("translationX", 0f, -2000f))
+        val translateY = ObjectAnimator.ofPropertyValuesHolder(
+                target, PropertyValuesHolder.ofFloat("translationY", 0f, 500f))
+        translateX.startDelay = 100
+        translateY.startDelay = 100
+        translateX.duration = 500
+        translateY.duration = 500
+        val cardAnimationSet = AnimatorSet()
+        cardAnimationSet.playTogether(rotation, translateX, translateY)
+
+        val overlayAnimator = ObjectAnimator.ofFloat(targetOverlay, "alpha", 0f, 1f)
+        overlayAnimator.duration = 200
+        val overlayAnimationSet = AnimatorSet()
+        overlayAnimationSet.playTogether(overlayAnimator)
+
+        activity_main_card_stack_view.swipe(SwipeDirection.Left, cardAnimationSet, overlayAnimationSet)
+    }
+
+    fun swipeRight() {
+        val spots = extractRemainingTouristSpots()
+        if (spots.isEmpty()) {
+            return
+        }
+
+        val target = activity_main_card_stack_view.getTopView()
+        val targetOverlay = activity_main_card_stack_view.getTopView().overlayContainer
+
+        val rotation = ObjectAnimator.ofPropertyValuesHolder(
+                target, PropertyValuesHolder.ofFloat("rotation", 10f))
+        rotation.duration = 200
+        val translateX = ObjectAnimator.ofPropertyValuesHolder(
+                target, PropertyValuesHolder.ofFloat("translationX", 0f, 2000f))
+        val translateY = ObjectAnimator.ofPropertyValuesHolder(
+                target, PropertyValuesHolder.ofFloat("translationY", 0f, 500f))
+        translateX.startDelay = 100
+        translateY.startDelay = 100
+        translateX.duration = 500
+        translateY.duration = 500
+        val cardAnimationSet = AnimatorSet()
+        cardAnimationSet.playTogether(rotation, translateX, translateY)
+
+        val overlayAnimator = ObjectAnimator.ofFloat(targetOverlay, "alpha", 0f, 1f)
+        overlayAnimator.duration = 200
+        val overlayAnimationSet = AnimatorSet()
+        overlayAnimationSet.playTogether(overlayAnimator)
+
+        activity_main_card_stack_view.swipe(SwipeDirection.Right, cardAnimationSet, overlayAnimationSet)
+    }
+
+    private fun reverse() {
+        activity_main_card_stack_view.reverse()
+    }
+
 }
---
+++ app/src/main/java/lt/getpet/getpet/PetAdapter.kt
@@ -0,0 +1,50 @@
+package lt.getpet.getpet
+
+import android.content.Context
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import android.support.design.widget.CoordinatorLayout.Behavior.setTag
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+
+
+class PetAdapter(context: Context) : ArrayAdapter<Pet>(context, 0) {
+
+    override fun getView(position: Int, contentView: View?, parent: ViewGroup): View {
+        var contentView = contentView
+        val holder: ViewHolder
+
+        if (contentView == null) {
+            val inflater = LayoutInflater.from(context)
+            contentView = inflater.inflate(R.layout.pet_card, parent, false)
+            holder = ViewHolder(contentView!!)
+            contentView!!.tag = holder
+        } else {
+            holder = contentView!!.tag as ViewHolder
+        }
+
+        val spot = getItem(position)
+
+        holder.name.setText(spot!!.name)
+        holder.short_description.setText(spot!!.short_description)
+        Glide.with(context).load(spot!!.url).into(holder.image)
+
+        return contentView
+    }
+
+    private class ViewHolder(view: View) {
+        var name: TextView
+        var short_description: TextView
+        var image: ImageView
+
+        init {
+            this.name = view.findViewById(R.id.pet_name)
+            this.short_description = view.findViewById(R.id.pet_short_description)
+            this.image = view.findViewById(R.id.pet_image) as ImageView
+        }
+    }
+
+}

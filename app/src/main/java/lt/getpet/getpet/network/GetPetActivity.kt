75fc70fd028350347eb9b071198e07f17e08ac8a
GretaGri <greta.klimaite@gmail.com>
2018-10-21 11:46:13 +0300

    Added Get_pet_activity, font.

--- app/src/main/java/lt/getpet/getpet/network/GetPetActivity.kt
+++ app/src/main/java/lt/getpet/getpet/network/GetPetActivity.kt
@@ -2,6 +2,9 @@ package lt.getpet.getpet.network
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_get_pet.*
+import kotlinx.android.synthetic.main.activity_pet_profile.*
 import lt.getpet.getpet.R
 import lt.getpet.getpet.data.PetResponse
 
@@ -14,6 +17,13 @@ class GetPetActivity : AppCompatActivity() {
 
         val pet = intent.getParcelableExtra<PetResponse>("pet")
 
+        Glide.with(this).load(pet.photo).into(get_pet_image)
+        get_pet_name.text = pet.name
+        get_pet_short_description.text = pet.short_description
 
+
+        shelter_name.text = pet.shelter.name
+        shelter_email.text = pet.shelter.email
+        shelter_phone.text = pet.shelter.phone
     }
 }

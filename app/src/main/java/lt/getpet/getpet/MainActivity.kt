9491bfccf0a37a8923d997f5b2f3f9c8fc149f41
Karolis Vycius <k.vycius@gmail.com>
2018-10-23 11:33:08 +0300

    Tabs refactor

--- app/src/main/java/lt/getpet/getpet/MainActivity.kt
+++ app/src/main/java/lt/getpet/getpet/MainActivity.kt
@@ -3,14 +3,17 @@ package lt.getpet.getpet
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import kotlinx.android.synthetic.main.activity_main.*
 import lt.getpet.getpet.managers.ManageFavourites
-import com.google.android.material.tabs.TabLayout.*
 import io.reactivex.disposables.Disposable
 import lt.getpet.getpet.fragments.PetFavoritesActivityFragment
 import lt.getpet.getpet.fragments.ProfileFragment
 import lt.getpet.getpet.fragments.SwipeFragment
 import lt.getpet.getpet.fragments.UserLoginFragment
+import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.tabs.TabLayout
+
 
 class MainActivity : AppCompatActivity(), UserLoginFragment.UserLoginCallback {
 
@@ -23,23 +26,11 @@ class MainActivity : AppCompatActivity(), UserLoginFragment.UserLoginCallback {
 
         ManageFavourites(context = applicationContext).clear()
 
-        replaceFragment(SwipeFragment.newInstance(), TAG_FRAGMENT_SWIPE)
+        view_pager.adapter = TabsPagerAdapter(supportFragmentManager)
 
-        tabs.addOnTabSelectedListener(object : OnTabSelectedListener {
-            override fun onTabReselected(tab: Tab) {
-                setTab(tab.position)
-            }
-
-            override fun onTabUnselected(tab: Tab) {
-                supportFragmentManager.findFragmentByTag(TAG_FRAGMENT_SWIPE)
-
-            }
-
-            override fun onTabSelected(tab: Tab) {
-                setTab(tab.position)
-            }
-
-        })
+        tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(view_pager))
+        view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+        view_pager.currentItem = 1
     }
 
     override fun onDestroy() {
@@ -47,54 +38,33 @@ class MainActivity : AppCompatActivity(), UserLoginFragment.UserLoginCallback {
         subscription?.dispose()
     }
 
-    fun setTab(position: Int) {
-        when (position) {
-            0 -> {
-                if (!isLoggedIn) {
-                    val fragment = supportFragmentManager.findFragmentByTag(TAG_USER_LOGIN_FRAGMENT)
-                            ?: UserLoginFragment.newInstance()
-
-                    replaceFragment(fragment, TAG_USER_LOGIN_FRAGMENT)
-                } else {
-                    val fragment = supportFragmentManager.findFragmentByTag(TAG_PROFILE_FRAGMENT)
-                            ?: ProfileFragment.newInstance()
-
-                    replaceFragment(fragment, TAG_PROFILE_FRAGMENT)
-                }
-            }
-            1 -> {
-                val fragment = supportFragmentManager.findFragmentByTag(TAG_FRAGMENT_SWIPE)
-                        ?: SwipeFragment.newInstance()
-
-                replaceFragment(fragment, TAG_FRAGMENT_SWIPE)
-            }
-            2 -> {
-                val fragment = supportFragmentManager.findFragmentByTag(TAG_FRAGMENT_FAVORITE_PETS)
-                        ?: PetFavoritesActivityFragment.newInstance()
-
-                replaceFragment(fragment, TAG_FRAGMENT_FAVORITE_PETS)
-            }
-        }
-    }
-
-    fun replaceFragment(fragment: Fragment, tag: String) {
-        val fm = supportFragmentManager
-        val ft = fm.beginTransaction()
-        ft.replace(R.id.frame_container, fragment, tag)
-        ft.addToBackStack(null)
-        ft.commit()
-    }
-
     override fun onUserLoggedIn() {
         isLoggedIn = true
-        setTab(0)
+        view_pager.adapter!!.notifyDataSetChanged()
     }
 
-    companion object {
-        const val TAG_FRAGMENT_SWIPE = "TAG_FRAGMENT_SWIPE"
-        const val TAG_FRAGMENT_FAVORITE_PETS = "TAG_FRAGMENT_FAVORITE_PETS"
-        const val TAG_USER_LOGIN_FRAGMENT = "TAG_USER_LOGIN_FRAGMENT"
-        const val TAG_PROFILE_FRAGMENT = "TAG_PROFILE_FRAGMENT"
+    inner class TabsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            return when (position) {
+                0 -> {
+                    if (isLoggedIn) {
+                        ProfileFragment.newInstance()
+                    } else {
+                        UserLoginFragment.newInstance()
+                    }
+                }
+                1 -> SwipeFragment.newInstance()
+                2 -> PetFavoritesActivityFragment.newInstance()
+                else -> throw IllegalArgumentException("Tab for position $position doesn't exist")
+
+            }
+        }
+
+        override fun getCount(): Int {
+            return 3
+        }
+
     }
 
 }

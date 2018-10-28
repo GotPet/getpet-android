c410e5596015271a669bb31dcf65ec1266334fc9
Karolis Vycius <k.vycius@gmail.com>
2018-10-28 14:23:44 +0200

    Increase speed for debug builds

--- app/src/main/java/lt/getpet/getpet/App.kt
+++ app/src/main/java/lt/getpet/getpet/App.kt
@@ -5,15 +5,22 @@ import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
 import lt.getpet.getpet.utils.RemoteCrashReportingTree
 import timber.log.Timber
+import com.crashlytics.android.core.CrashlyticsCore
+
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        val crashlyticsKit = Crashlytics.Builder()
+                .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build()
+
+        Fabric.with(this, crashlyticsKit)
+
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         } else {
-            Fabric.with(this, Crashlytics())
             Timber.plant(RemoteCrashReportingTree())
         }
     }

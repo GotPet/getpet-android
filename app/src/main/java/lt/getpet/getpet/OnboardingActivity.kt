d35f792edad158f58139c4049f26d510420ecc67
Greta Gri <greta.klimaite@gmail.com>
2018-11-11 00:40:18 +0200

    Polished OnBoardingFragment: changed if to more Kolin when, added exception and moved saving info to shared preferences when on boarding is finished to OnBoardingActivity.

--- app/src/main/java/lt/getpet/getpet/OnboardingActivity.kt
+++ app/src/main/java/lt/getpet/getpet/OnboardingActivity.kt
@@ -1,12 +1,13 @@
 package lt.getpet.getpet
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_onboarding.*
 import lt.getpet.getpet.adapters.OnboardingAdapter
 import lt.getpet.getpet.navigation.NavigationManager
 import javax.inject.Inject
 import androidx.viewpager.widget.ViewPager
-
+import lt.getpet.getpet.constants.ActivityConstants
 
 
 class OnboardingActivity : BaseActivity() {
@@ -38,7 +39,15 @@ class OnboardingActivity : BaseActivity() {
         })
 
         button_next.setOnClickListener {
-            if (onboarding_viewpager.currentItem == 4) showMainActivity()
+            if (onboarding_viewpager.currentItem == 4){
+            // User has seen OnboardingFragment, so mark our SharedPreferences
+            // flag as completed so that we don't show our OnboardingFragment
+            // the next time the user launches the app.
+                PreferenceManager.getDefaultSharedPreferences(this).edit().apply {
+                    putBoolean(ActivityConstants.COMPLETED_ONBOARDING_PREF_NAME, true)
+                    apply()}
+            // End on boarding, go to main activity
+                showMainActivity()}
             else onboarding_viewpager.setCurrentItem(onboarding_viewpager.currentItem + 1, true)
         }
     }
--- app/src/main/java/lt/getpet/getpet/fragments/OnBoardingFragment.kt
+++ app/src/main/java/lt/getpet/getpet/fragments/OnBoardingFragment.kt
@@ -1,15 +1,14 @@
 package lt.getpet.getpet.fragments
 
-import android.preference.PreferenceManager
-import lt.getpet.getpet.constants.ActivityConstants.Companion.COMPLETED_ONBOARDING_PREF_NAME
 import android.os.Bundle
-import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.VISIBLE
+import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_onboarding.view.*
 import lt.getpet.getpet.R
 import lt.getpet.getpet.constants.ActivityConstants.Companion.POSITION
+import java.lang.IndexOutOfBoundsException
 
 
 /**
@@ -22,46 +21,44 @@ class OnBoardingFragment : BaseFragment() {
 
         val rootView = inflater.inflate(R.layout.fragment_onboarding, container, false)
 
-        val mPosition = getArguments()!!.getInt(POSITION);
-        if (mPosition == 0) {
-            rootView.onboarding_image.setImageResource(R.drawable.onboarding_one)
-            rootView.onboarding_text_heading.setText(R.string.onboarding_1_heading)
-            rootView.onboarding_text_main.setText(R.string.onboarding_1_text)
+        val position = getArguments()!!.getInt(POSITION)
+        when (position){
+            0 -> {
+                rootView.onboarding_image.setImageResource(R.drawable.onboarding_one)
+                rootView.onboarding_text_heading.setText(R.string.onboarding_1_heading)
+                rootView.onboarding_text_main.setText(R.string.onboarding_1_text)
+            }
 
-        } else if (mPosition == 1) {
-            rootView.toolbar.visibility = VISIBLE
-            rootView.onboarding_image.setImageResource(R.drawable.onboarding_two)
-            rootView.onboarding_text_heading.setText(R.string.onboarding_2_heading)
-            rootView.onboarding_text_main.setText(R.string.onboarding_2_text)
+            1 -> {
+                rootView.toolbar.visibility = VISIBLE
+                rootView.onboarding_image.setImageResource(R.drawable.onboarding_two)
+                rootView.onboarding_text_heading.setText(R.string.onboarding_2_heading)
+                rootView.onboarding_text_main.setText(R.string.onboarding_2_text)
+            }
 
-        } else if (mPosition == 2) {
-            rootView.onboarding_image.setImageResource(R.drawable.onboarding_tree)
-            rootView.onboarding_text_heading.setText(R.string.onboarding_3_heading)
-            rootView.onboarding_text_main.setText(R.string.onboarding_3_text)
+            2 -> {
+                rootView.onboarding_image.setImageResource(R.drawable.onboarding_tree)
+                rootView.onboarding_text_heading.setText(R.string.onboarding_3_heading)
+                rootView.onboarding_text_main.setText(R.string.onboarding_3_text)
+            }
 
-        } else if (mPosition == 3) {
-            rootView.onboarding_image.setImageResource(R.drawable.onboarding_four)
-            rootView.onboarding_text_heading.setText(R.string.onboarding_4_heading)
-            rootView.onboarding_text_main.setText(R.string.onboarding_4_text)
+            3 -> {
+                rootView.onboarding_image.setImageResource(R.drawable.onboarding_four)
+                rootView.onboarding_text_heading.setText(R.string.onboarding_4_heading)
+                rootView.onboarding_text_main.setText(R.string.onboarding_4_text)
+            }
 
-        } else if (mPosition == 4) {
-            rootView.onboarding_image.setImageResource(R.drawable.onboarding_five)
-            rootView.onboarding_text_heading.setText(R.string.onboarding_5_heading)
-            rootView.onboarding_text_main.setText(R.string.onboarding_5_text)
+            4 -> {
+                rootView.onboarding_image.setImageResource(R.drawable.onboarding_five)
+                rootView.onboarding_text_heading.setText(R.string.onboarding_5_heading)
+                rootView.onboarding_text_main.setText(R.string.onboarding_5_text)
+            }
 
+            else -> {val indexOutOfBoundsException = IndexOutOfBoundsException()
+                throw indexOutOfBoundsException
+            }
         }
 
         return rootView
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        // User has seen OnboardingFragment, so mark our SharedPreferences
-        // flag as completed so that we don't show our OnboardingFragment
-        // the next time the user launches the app.
-        PreferenceManager.getDefaultSharedPreferences(context).edit().apply {
-            putBoolean(COMPLETED_ONBOARDING_PREF_NAME, true)
-            apply()
-        }
     }
 }

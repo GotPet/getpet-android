1e43838359389a2b516b3c0a66375530d20c92a6
Greta Gri <greta.klimaite@gmail.com>
2018-11-10 21:32:08 +0200

    Fixed onboarding button text change, to react to swipe changes too.

--- app/src/main/java/lt/getpet/getpet/OnboardingActivity.kt
+++ app/src/main/java/lt/getpet/getpet/OnboardingActivity.kt
@@ -5,6 +5,9 @@ import kotlinx.android.synthetic.main.activity_onboarding.*
 import lt.getpet.getpet.adapters.OnboardingAdapter
 import lt.getpet.getpet.navigation.NavigationManager
 import javax.inject.Inject
+import androidx.viewpager.widget.ViewPager
+
+
 
 class OnboardingActivity : BaseActivity() {
 
@@ -21,9 +24,20 @@ class OnboardingActivity : BaseActivity() {
 
         indicator.setViewPager(onboarding_viewpager)
 
+        onboarding_viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+               if(position == 4) button_next.setText(getString(R.string.button_end_onboarding))
+                else button_next.setText(getString(R.string.button_next))
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+        })
 
         button_next.setOnClickListener {
-            if (onboarding_viewpager.currentItem == 3) button_next.setText(getString(R.string.button_end_onboarding))
             if (onboarding_viewpager.currentItem == 4) showMainActivity()
             else onboarding_viewpager.setCurrentItem(onboarding_viewpager.currentItem + 1, true)
         }
